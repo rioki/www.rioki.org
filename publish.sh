a9d5a1f77ef8f446034250afddbc60e2a383a77e
Sean Farrell <sean.farrell@rioki.org>
2013-03-01 17:43:17 +0100

    Fixes publish script so cygwin won't mess with the permissions.

--- publish.sh
+++ publish.sh
@@ -3,5 +3,5 @@
 set -x
 
 cd _site
-rsync -r * root@eris.rioki.org:/var/www/www.rioki.org/htdocs/
+rsync -r --chmod=ug=rwX,o=rX -p * root@eris.rioki.org:/var/www/www.rioki.org/htdocs/
 ssh root@eris.rioki.org "cd /var/www/www.rioki.org/htdocs/ && chown -R www-data:www-data *"
